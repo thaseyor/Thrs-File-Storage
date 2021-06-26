d23e756bcc7418996fda4f1dfaba060a414474a2
thaseyor <56724794+thaseyor@users.noreply.github.com>
2021-06-26 19:04:52 +0600

    fix: pwa settings

--- quasar.conf.js
+++ quasar.conf.js
@@ -113,13 +113,13 @@ module.exports = function(ctx) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Thsr-file-storage`,
+        short_name: `Thsr`,
         description: `File storage`,
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#212121',
         icons: [
           {
             src: 'icons/icon-128x128.png',
